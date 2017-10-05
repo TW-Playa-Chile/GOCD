ad1c1c5bddafa31b053503fb958cc7d8ef98017d
Yerko Aguirre <yaguirre@thoughtworks.com>
2017-10-05 15:22:45 -0300

    Refactoring code and updating yaml plugin

--- gocd/pythonAgent/resources/autoregister.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-(
-cat <<EOF
-agent.auto.register.key=${GO_AGENT_AUTOREGISTER_KEY}
-agent.auto.register.resources=${GO_AGENT_AUTOREGISTER_RESOURCE}
-agent.auto.register.hostname=${GO_AGENT_AUTOREGISTER_RESOURCE}_agent
-EOF
-) > $GO_AGENT_DIR/config/autoregister.properties
